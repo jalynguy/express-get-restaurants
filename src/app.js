cff603245cf2268ccbd5544a42f7421956cbc8f6
James Nguyen <james.nguyen@james.nguyen-MacBook-Pro>
2024-10-30 13:50:40 -0700

    adding app functions

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below: 
 
-
 app.get('/restaurants', async (req, res)=>{
     const result = await Restaurant.findAll();
     res.send(result).json();
@@ -17,4 +16,16 @@ app.get('/restaurants/:id', async (req, res) => {
     res.send(result).json();
 });
 
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.post('/restaurants', async (req,res)=>{
+    const result = await Restaurant.create({name: 'Kendrick Lamar', instrument: 'Voice'});
+});
+
+app.put('/restaurants/:id', async (req, res)=>{
+    const id = req.params.id;
+    const result = await Restaurant.update();
+})
 module.exports = app;
